4145f8dedc72010c3e76a5c35cbbb040a59c26fc
taqu <taqu2920.dev@gmail.com>
2024-02-01 00:28:03 +0900

    set the project settings
    add settings for embeddings

--- one_scrap.py
+++ one_scrap.py
@@ -6,6 +6,7 @@ import re
 import string
 import requests
 import json
+import hashlib
 
 from scrapy.spiders import SitemapSpider
 from scrapy.exceptions import CloseSpider
@@ -24,15 +25,16 @@ class SiteSpider(SitemapSpider):
         'LOG_LEVEL': 'WARNING',
     }
 
-    def __init__(self,site:string,rule:string,chunk_size:int, chunk_overlap:int, collection:string):
+    def __init__(self, site:string, rule:string, db_host:string, db_port:int, chunk_size:int, chunk_overlap:int, collection:string, embedding_address:string):
         super().__init__()
         if site:
             self.sitemap_urls = [site]
         if rule:
             self.sitemap_rules = [(rule,'parse')]
         self.text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-        self.chroma_client = chromadb.HttpClient(port='8090')
+        self.chroma_client = chromadb.HttpClient(host=db_host,port=db_port)
         self.collection = self.chroma_client.get_or_create_collection(name=collection)
+        self.embedding_url = embedding_address + '/v1/embeddings'
 
     def parse(self, response):
         document = ''
@@ -42,39 +44,45 @@ class SiteSpider(SitemapSpider):
                 continue
             document += line + '\n'
         chunks = self.text_splitter.split_text(document)
-        url = 'http://localhost:8000/v1/embeddings'
         headers = {'Content-Type': 'application/json'}
+
+        count = 0
         for chunk in chunks:
+
+            chunk_id = hashlib.sha1(('{0}{1:08}'.format(response.url, count)).encode()).hexdigest()
+            count = count + 1
             payload = {
                 'input': chunk,
                 'model': 'multilingual-e5-large',
                 'encoding_format': 'float'}
-            response = requests.post(url, headers=headers, json=payload)
+            response = requests.post(self.embedding_url, headers=headers, json=payload)
             if 200 != response.status_code:
                 continue
             embedding = response.json()['data'][0]['embedding']
-            request = {
-                'embeddings': [embedding],
-                'metadatas': [{'url': response.url}],
-                'documents': [chunk],
-            }
-            chroma_response = self.collection.upsert(request)
+            ids = [chunk_id]
+            embeddings = [embedding]
+            metadatas = [{'url': response.url}]
+            documents = [chunk]
+            chroma_response = self.collection.upsert(ids=ids,embeddings=embeddings,documents=documents,metadatas=metadatas)
 
-def run_scrapy(site:string, rule:string, chunk_size:int, chunk_overlap:int, collection:string):
+def run_scrapy(site:string, rule:string, db_host:string, db_port:int, chunk_size:int, chunk_overlap:int, collection:string, embedding_address:string):
     if not site:
         return
 
     process = CrawlerProcess()
-    process.crawl(SiteSpider, site=site, rule=rule,chunk_size=chunk_size,chunk_overlap=chunk_overlap,collection=collection)
+    process.crawl(SiteSpider, site=site, rule=rule,db_host=db_host,db_port=db_port,chunk_size=chunk_size,chunk_overlap=chunk_overlap,collection=collection,embedding_address=embedding_address)
     process.start()
 
 def main():
     sg.theme('DarkAmber')
     layout = [[sg.Text('Site'), sg.InputText(default_text='https://docs.unrealengine.com/sitemapindex.xml')],
               [sg.Text('Rule'), sg.InputText(default_text='/5.3/')],
+              [sg.Text('Vector DB Host'), sg.InputText(default_text='localhost')],
+              [sg.Text('Vector DB Port'), sg.InputText(default_text='8000')],
               [sg.Text('Chunk Size'), sg.InputText(default_text='480')],
               [sg.Text('Chunk Overlap'), sg.InputText(default_text='48')],
               [sg.Text('Collection'), sg.InputText(default_text='unreal_doc')],
+              [sg.Text('Embedding Address'), sg.InputText(default_text='http://localhost:8090')],
               [sg.Button('Run')]]
 
     window = sg.Window('Window Title', layout)
@@ -83,7 +91,7 @@ def main():
         if event == sg.WIN_CLOSED:
             break
         if event == 'Run':
-            run_scrapy(values[0],values[1], int(values[2]), int(values[3]), values[4])
+            run_scrapy(values[0],values[1], values[2], int(values[3]), int(values[4]), int(values[5]), values[6], values[7])
             continue
 
     window.close()
